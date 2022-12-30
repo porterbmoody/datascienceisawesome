58e171d2706257a876e1b0a668ec2d3448182a0a
Porter Moody <pmoody@awolff.com>
2022-12-30 14:11:44 -0700

    F

---
+++ _common.R
@@ -0,0 +1,24 @@
+set.seed(1014)
+options(digits = 3)
+
+library(reticulate)
+library(ggplot2)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  cache = FALSE,
+  engine.path = '/usr/local/bin/python3.8',
+  python.reticulate = TRUE,
+  message = FALSE,
+  warning = FALSE
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
